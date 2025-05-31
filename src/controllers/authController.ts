d99a2dfc4cc7ac481791def722be288b38eb7af1
Sathvika K <123883498+sathvikak255@users.noreply.github.com>
2025-05-31 21:09:13 +0530

    Update authController.ts

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,4 +1,4 @@
-import User from "../models/User.ts";
+import User from "../models/User.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import { Request, Response } from "express";
