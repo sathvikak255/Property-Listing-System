816fea37093e66044951149d100daba813b79b6c
Sathvika K <123883498+sathvikak255@users.noreply.github.com>
2025-05-31 21:03:42 +0530

    Initial Commit

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,4 +1,4 @@
-import User from "../models/User.js";
+import User from "../models/User.ts";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import { Request, Response } from "express";
